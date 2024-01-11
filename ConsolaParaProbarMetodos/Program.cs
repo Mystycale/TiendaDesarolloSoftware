7b143eb73c1835e6c4eeda7e96e241fa978158db
mystyc <holisoysub@gmail.com>
2024-01-11 14:44:45 -0400

    Se agrega el webservice y los logs

---
+++ ConsolaParaProbarMetodos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaParaProbarMetodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.ServicioCapaSoapClient metodos = new ServiceReference1.ServicioCapaSoapClient();
+
+            string respuesta = metodos.HelloWorld(); //llamando al metodo hello world
+            Console.WriteLine(respuesta);
+            //recordar que cuando se agregue o se actualice un metodo web
+            //se debe de dar click derecho sobre las serviceReference1 (adentro de connected services) y...
+            //Click en actualizar referencias
+
+        }
+    }
+}
---
+++ ServicioW/ServicioCapa.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ServicioW
+{
+    /// <summary>
+    /// Descripción breve de ServicioCapa
+    /// </summary>
+    [WebService(Namespace = "http://intec.edu.do")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+
+    public class ServicioCapa : System.Web.Services.WebService
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
+        [WebMethod]
+        public string HelloWorld()
+        {
+            log.Info("Prueba"); //Los log van siempre antes del return
+
+            return "Hola a todos";
+            
+        }
+    }
+}
