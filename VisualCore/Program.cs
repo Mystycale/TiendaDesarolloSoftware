3cc9878a3d339de280754cc66a084c8deb04d46d
mystyc <holisoysub@gmail.com>
2024-01-12 12:46:49 -0400

    Creacion del webServiceCore con log4net incluido

---
+++ VisualCore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicioDelCore.CoreWebServiceSoapClient cliente = new ServicioDelCore.CoreWebServiceSoapClient(); 
+
+            string respuesta = cliente.HelloWorld(); //Helloworld es uno de los metodos del servicio web
+            //Recuerda que cada vez que se agregues un nuevo metodo en el servicio web, se debe...
+            //dar click derecho sobre "ServicioDelCore"(esta adentro de "Connected Services en visualCore") y...
+            //Click en actualizar referencia del servicio
+
+            Console.WriteLine(respuesta);
+        }
+    }
+}
---
+++ WebCore/CoreWebService.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebCore
+{
+    /// <summary>
+    /// Descripción breve de CoreWebService
+    /// </summary>
+    [WebService(Namespace = "http://google.com")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CoreWebService : System.Web.Services.WebService
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            log.Info("Prueba del log"); // El log va antes del return
+            //log.Debug("");
+            //log.Error("");
+            return "Hola a todos";
+        }
+    }
+}
