e8198aa1cf9d8e85df18d92507d018e1312856a3
GREGORY JOSE PEGUERO REYES <1115271@est.intec.edu.do>
2024-01-13 21:33:01 -0400

    PRODUCTO creada CAJERO crada SERVICIO creada CLIENTE creada FACTURA creada FCT_PROD creada FCT_SERV creada

--- VisualCore/Program.cs
+++ VisualCore/Program.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+PRODUCTO creada
+CAJERO crada
+SERVICIO creada
+CLIENTE creada
+FACTURA creada
+FCT_PROD creada
+FCT_SERV creada
+*/
 
 namespace VisualCore
 {
@@ -15,7 +24,7 @@ namespace VisualCore
             string respuesta = cliente.HelloWorld(); //Helloworld es uno de los metodos del servicio web
             //Recuerda que cada vez que se agregues un nuevo metodo en el servicio web, se debe...
             //dar click derecho sobre "ServicioDelCore"(esta adentro de "Connected Services en visualCore") y...
-            //Click en actualizar referencia del servicio NADA
+            //Click en actualizar referencia del servicio 
 
             Console.WriteLine(respuesta);
         }
